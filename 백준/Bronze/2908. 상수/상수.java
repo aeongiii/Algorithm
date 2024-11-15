d2ce9b2cadd81fb9836fe6693e65cb294502a50d
aeongiii <151815152+aeongiii@users.noreply.github.com>
2024-11-15 11:46:43 +0900

    [Bronze II] Title: 상수, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,36 +1,39 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+1. 문제 분석
+    1) 공백으로 구분된 세 자리 수 2개가 주어진다.
+    2) 각 수를 뒤집은 뒤 큰 수를 출력한다.
+
+2. 제약 조건
+    두 수는 같지 않은 세 자리 수이며, 0이 포함되어 있지 않다.
+
+3. 의사결정
+    1) StringTokenizer로 세 자리 수 2개를 입력받는다.
+    2) 각 수를 뒤집은 뒤 비교하여 큰 수를 출력한다.
+
+4. 문제 해결
+    1) String 대신 StringBuffer 또는 StringBuilder를 사용하면 문자열을 편집할 수 있다.
+       StringBuffer는 멀티쓰레드에 안전하도록 설정되어있으므로,
+       멀티쓰레드로 작성된 프로그램이 아닌 경우 StringBuilder를 사용하는 것이 성능 면에서 좋다.
+    2) if문을 사용해 큰 수를 비교하는 것보다 Math.max를 사용하는 것이 좋다.
+ */
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		ArrayList<Integer> intlist = new ArrayList<Integer>();
-		int t = 0;
-		while (true) {
-			int num = scanner.nextInt();
-			
-			String num1 = Integer.toString(num%10);
-			String num2 = Integer.toString(num/10%10);
-			String num3 = Integer.toString(num/100%10);
-			
-			String reverseNum = num1 + num2 + num3;
-			
-			intlist.add(Integer.parseInt(reverseNum));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-			t ++;
-			
-			if (t == 2) {
-				break;
-			}
-		}
-			int A = intlist.get(0);
-			int B = intlist.get(1);
-		
-			if (A>B) {
-				System.out.println(A);
-			} else {
-				System.out.println(B);
-			}
-	}
+        // 토큰 받아서 > StringBuilder 클래스로 변환 > reverse() > toString() > int로 변환
+        int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        // Math 라이브러리를 이용해서 큰 수 출력
+        System.out.println(Math.max(A,B));
+
+    }
 }
