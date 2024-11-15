860679c5fc6830b747cf22a5947d448027149139
aeongiii <151815152+aeongiii@users.noreply.github.com>
2024-11-15 11:21:53 +0900

    [Bronze II] Title: 단어의 개수, Time: 248 ms, Memory: 20172 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,20 +1,36 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+1. 문제 분석
+    1) 영어 대소문자 + 공백으로 이루어진 문자열이 주어진다.
+    2) 단어는 공백으로 구분된다.
+    3) 단어의 개수를 세서 출력한다.
+    4) 문자열의 처음과 끝에 공백이 들어갈 수 있다.
+
+2. 제약 조건
+    문자열의 길이 <= 1,000,000
+
+3. 의사결정
+    1) 문자열 S를 입력받는다.
+    2) StringTokenizer를 사용해 공백 기준으로 분리한다.
+    4) 토큰 길이를 출력한다.
+
+4. 문제 해결
+    1) countTokens() : StringTokenizer가 가지고 있는 토큰의 개수 반환
+    2) split()은 무조건 하나 이상의 배열을 출력하기 때문에 문자열에 공백만 있을 경우 1을 반환하지만
+       StringTokenizer의 경우 0을 반환하기 때문에 더 적절하다.
+ */
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		String s = scanner.nextLine();
-		String S = s.trim();	// 입력받은 문자열 앞뒤 공백 제거
-		
-		if (s.equals(" ") || s.isEmpty()==true) {
-			System.out.println(0);    // 공백만 있거나 비었을 경우 0
-		}
-		else {
-			String[] arr = S.split(" ");	// 공백 단위로 문자열 자르기
-			System.out.println(arr.length);	// 길이 출력
-		}
-	}
+        System.out.println(st.countTokens());
+
+    }
 }
