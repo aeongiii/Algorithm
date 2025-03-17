9c0a3e812fb222bb6a085a5d9fde1ace60bd261c
Ye Eun Kim <151815152+aeongiii@users.noreply.github.com>
2025-03-17 21:11:45 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 172 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        long n = sc.nextInt()-2;
+ 
+        System.out.println(((n*n*n)+(3*n*n)+(2*n))/6);
+        System.out.println(3);
+        
+    }
+}
